a60108c1d5198a5a7f316bedd6798f0358d0187b
Luca Pattocchio <pattocchioluca@gmail.com>
2025-12-16 00:09:47 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# omakube.omakasui.org
+
+This is the [`omakube`](https://github.com/Kasui92/omakube) website.
+
+## Contributions
+
+This is a personal project, but suggestions and feedback are always welcome!
+
+## License
+
+omakube.omakasui.org is released under the [MIT License](https://github.com/Kasui92/omakube.omakasui.org/blob/main/LICENSE).
---
+++ astro.config.mjs
@@ -0,0 +1,39 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import path from "path";
+import { fileURLToPath } from "url";
+
+import tailwindcss from "@tailwindcss/vite";
+import { remarkAlert } from "remark-github-blockquote-alert";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://omakube.omakasui.org",
+  base: "/",
+  vite: {
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+    build: {
+      sourcemap: false,
+      rollupOptions: {
+        output: {
+          sourcemap: false,
+        },
+      },
+    },
+  },
+  markdown: {
+    remarkPlugins: [remarkAlert],
+    rehypePlugins: [],
+    shikiConfig: {
+      theme: "github-dark",
+      wrap: true,
+    },
+  },
+});
